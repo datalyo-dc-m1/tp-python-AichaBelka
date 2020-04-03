7361ce4f20e9b927549c92a3eea4451e1d6d985c
AichaBelka <63093607+AichaBelka@users.noreply.github.com>
2020-04-03 15:21:44 +0200

    exercice 0

---
+++ tp1/exercice 0.py
@@ -0,0 +1,13 @@
+a = 5.3
+b = 9.70
+c = a + b
+print(c)
+c += a # c= c+a
+d = c
+d -= 1 # D= d-1
+print(d)
+# C à une valeur de 15
+# D à une valeur de 19.3
+c_int=int(c)
+print(c_int)
+# C int = 20 car int permet d'arrondir et d'afficher des nombres entiers
